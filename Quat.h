deb477cb1014fd07adf778be01b3cfddd77e6b5c
buwqol <buwqol@gmail.com>
2026-06-29 11:19:59 +1000

    Tiny change.

--- Quat.h
+++ Quat.h
@@ -4,7 +4,7 @@
 typedef union { struct { tFP32 real; tFP32 i, j, k; }; tFP32 comp[4U]; } tFP32Quat;
 LINK_C_Begin
 ForceInline tFP32Quat tFP32Quat_Make(tFP32 real, tFP32 i, tFP32 j, tFP32 k);
-tFP32 tFP32Quat_MagSq(tFP32Quat quat);
+ForceInline tFP32 tFP32Quat_MagSq(tFP32Quat quat);
 ForceInline tFP32 tFP32Quat_Mag_iter(tFP32Quat quat, tIUSz itr);
 ForceInline tFP32 tFP32Quat_Mag(tFP32Quat quat);
 LINK_C_End
@@ -18,7 +18,7 @@ ForceInline tFP32Quat tFP32Quat_Make(tFP32 real, tFP32 i, tFP32 j, tFP32 k)
 	quat.k = k;
 	return quat;
 }
-tFP32 tFP32Quat_MagSq(tFP32Quat quat)
+ForceInline tFP32 tFP32Quat_MagSq(tFP32Quat quat)
 {
 	return tFP32_Sq(quat.real) + tFP32_Sq(quat.i) + tFP32_Sq(quat.j) + tFP32_Sq(quat.k);
 }
